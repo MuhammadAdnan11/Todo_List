c6fa3a741d2574b56222922a11f4e16b4ac91f02
Adnan khan <adnan191022@gmail.com>
2025-06-01 09:06:27 -0700

    added functionality

---
+++ todo.js
@@ -0,0 +1,31 @@
+let todoList=['buy milk','first task']
+displayItems();
+function addTodo(){
+  let inputElement=document.querySelector('#todo-input');
+  let todoItem= inputElement.value;
+
+ todoList.push(todoItem)
+inputElement.value=''
+
+displayItems();
+}
+
+function displayItems(){
+  let containerElement=document.querySelector('.todo-container');
+
+  let newHtml=''; 
+
+   for(let i=0; i<todoList.length; i++)
+  {
+
+    newHtml+=`
+    <div>
+    <span>${todoList[i]}</span>
+    <button>Delete</button>
+    </div>
+        `;
+
+}
+containerElement.innerHTML=newHtml;
+
+}
